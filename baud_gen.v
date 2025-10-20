bfcb1b4a40af2709d39c6dcbe4e55bf0012e0997
RohanRudra <rohanrudra0914@gmail.com>
2025-10-20 12:08:44 +0530

    Commit

--- baud_gen.v
+++ baud_gen.v
@@ -1,33 +1,3 @@
-// module baud_gen #(
-//     parameter CLK_FREQ = 50000000
-// )(
-//     input        clk,        
-//     input        resetn,
-//     input [31:0] baud_rate,
-//     output reg   baud_tick   
-// );
-//     integer COUNT_MAX;
-//     initial COUNT_MAX = CLK_FREQ / baud_rate;
-    
-//     reg [31:0] count;
-
-//     always @(posedge clk or negedge resetn) begin
-//         if (!resetn) begin
-//             count <= 0;
-//             baud_tick <= 0;
-//         end 
-//         else if (count == COUNT_MAX - 1) begin
-//             count <= 0;
-//             baud_tick <= 1;   // generate tick
-//         end 
-//         else begin
-//             count <= count + 1;
-//             baud_tick <= 0;
-//         end
-//     end
-// endmodule
-
-
 module baud_gen #(
     parameter CLK_FREQ = 50000000
 )(
